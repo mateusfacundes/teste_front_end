170b6cb3b41ffe233bb73a07256e1ceed7e36c5b
Mateus Facundes <mateusmarchao@gmail.com>
2023-02-02 12:14:42 -0300

    feet: State and city options menu

---
+++ src/components/DrowTable.js
@@ -0,0 +1,103 @@
+import React, {useState, useEffect} from 'react';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+
+const columns = [
+    { id: 'code', label: 'Codigo', minWidth: 170 },
+    { id: 'name', label: 'Nome da instituição', minWidth: 100 },
+    {
+        id: 'population',
+        label: 'Population',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'size',
+        label: 'Size\u00a0(km\u00b2)',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'density',
+        label: 'Density',
+        minWidth: 170,
+        align: 'right',
+        format: (value) => value.toFixed(2),
+    },
+];
+
+const rows = [
+
+];
+
+export default function DrowTable() {
+
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+
+    return (
+        <> 
+        <TableContainer sx={{ maxHeight: 440 }}>
+            <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                    <TableRow>
+                    {columns.map((column) => (
+                        <TableCell
+                        key={column.id}
+                        align={column.align}
+                        style={{ minWidth: column.minWidth }}
+                        >
+                        {column.label}
+                        </TableCell>
+                    ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                        return (
+                        <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                            {columns.map((column) => {
+                            const value = row[column.id];
+                            return (
+                                <>aasdsad</>
+                            );
+                            })}
+                        </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        <TablePagination
+            rowsPerPageOptions={[10, 25, 100]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+    </>
+    )
+}
--- src/pages/ShowSchouls.js
+++ src/pages/ShowSchouls.js
@@ -1,5 +1,11 @@
 import React, {useState, useEffect} from 'react';
 
+import axios from 'axios';
+
+import config from '../config.json'
+
+import DrowTable from '../components/DrowTable';
+
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
@@ -14,105 +20,66 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
+import Autocomplete from '@mui/material/Autocomplete';
+
 import { Button } from '@mui/material';
 
-
-
-const columns = [
-  { id: 'code', label: 'Codigo', minWidth: 170 },
-  { id: 'name', label: 'Nome da instituição', minWidth: 100 },
-  {
-    id: 'population',
-    label: 'Population',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
-  },
-  {
-    id: 'density',
-    label: 'Density',
-    minWidth: 170,
-    align: 'right',
-    format: (value) => value.toFixed(2),
-  },
-];
-
-// chamada da api
-async function getStates(){
-  const data = await fetch("http://localhost:5000/uf", {
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(response => JSON.stringify(response));
-
-
-  return data;
-}
-
-async function getSchouls(){
-  
-}
-
-
-const rows = [
-
-];
+const configAxios = {
+  headers:{
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json',
+  }
+};
 
 export default function ShowSchouls() {
-  
-  useEffect(() => {
-    const states = getStates();
-    console.log(states);
-  });
+  const allStates = config.uf
+  const [citys, setCitys] = useState([]);
 
-  const [state, setstate] = useState('');
+  async function getCitys(selectedState){
+    await axios.get(config.api_url+"/cidades/"+selectedState, configAxios)
+    .then((response) => setCitys(response.data))
+    console.log(citys);
+  }
+
+  async function getSchouls(){
+  
+  }
 
   const handleChangeState = (event) => {
-    setstate(event.target.value);
-  };
-
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
+    getCitys(event.target.value);
   };
 
   return (
     <Box sx={{wideth: 200, height: 200, display: 'flex', flexWrap: 'wrap' }}>
         <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-          
+    
             <h1>Escolas Cadastradas</h1>
             <FormControl>
-              <InputLabel id="demo-simple-select-label">Estado</InputLabel>
+              <InputLabel id="state-label">Estado</InputLabel>
               <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={state}
-                label="Age"
+                defaultValue = ""
+                labelId="state-label"
+                id="state-select"
+                label="States"
                 onChange={handleChangeState}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                {allStates.map((state) => {
+                  return (
+                    <MenuItem key={state.sigla} value={state.sigla}>{state.nome}</MenuItem>    
+                  )
+                })}
               </Select>
 
+              <Autocomplete
+                disablePortal
+                id="combo-box-demo"
+                options={citys}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="Movie" />}
+              />
+
               <TextField
-                label="Nome da cidade"
+                label="Nome da Escola"
                 id="outlined-start-adornment"
                 sx={{ m: 1, width: '25ch' }}
               />
@@ -121,52 +88,8 @@ export default function ShowSchouls() {
 
             </FormControl>
             
-        <TableContainer sx={{ maxHeight: 440 }}>
-            <Table stickyHeader aria-label="sticky table">
-            <TableHead>
-                <TableRow>
-                {columns.map((column) => (
-                    <TableCell
-                    key={column.id}
-                    align={column.align}
-                    style={{ minWidth: column.minWidth }}
-                    >
-                    {column.label}
-                    </TableCell>
-                ))}
-                </TableRow>
-            </TableHead>
-            <TableBody>
-                {rows
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row) => {
-                    return (
-                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
-                        {columns.map((column) => {
-                        const value = row[column.id];
-                        return (
-                            <TableCell key={column.id} align={column.align}>
-                            {column.format && typeof value === 'number'
-                                ? column.format(value)
-                                : value}
-                            </TableCell>
-                        );
-                        })}
-                    </TableRow>
-                    );
-                })}
-            </TableBody>
-            </Table>
-        </TableContainer>
-        <TablePagination
-            rowsPerPageOptions={[10, 25, 100]}
-            component="div"
-            count={rows.length}
-            rowsPerPage={rowsPerPage}
-            page={page}
-            onPageChange={handleChangePage}
-            onRowsPerPageChange={handleChangeRowsPerPage}
-        />
+            <DrowTable />
+
         </Paper>
     </Box>
   );
