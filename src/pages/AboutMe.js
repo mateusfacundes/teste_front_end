dc8723804bf7a0fc391a766bf0774ccb7e9bb368
Mateus Facundes <mateusmarchao@gmail.com>
2023-02-07 13:46:51 -0300

    fix: text formatation

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -9,7 +9,7 @@ import HikingOutlinedIcon from '@mui/icons-material/HikingOutlined';
 import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
 import { textAlign } from "@mui/system";
 
-const baseStyle = { width: '70%', margin: '30px auto', padding: '10px', paddingBottom: '40px'};
+const baseStyle = { width: '90%', margin: '30px auto', padding: '20px', paddingBottom: '40px'};
 const paperStyle={height:'80%'}
 
 export default function AboutMe() {
@@ -29,8 +29,8 @@ export default function AboutMe() {
             <Grid xs={12} md={4}>
                 <Paper sx={baseStyle} style={paperStyle}>
                     <h1>Sobre mim</h1>
-                    <p style={{textAlign: 'justify'}}>
-                        Meu nome é <b>Mateus Marchão</b>, sou formado em <b>ciência da computação</b> sou programador e pretendo sempre estudar e evoluir nesta area. 
+                    <p style={{textAlign: 'justify', padding: '10px', textIndent: '25px'}}>
+                        Meu nome é <b>Mateus Marchão</b>, sou formado em <b>ciência da computação</b> sou programador e pretendo sempre estudar e evoluir nesta área. 
                     </p>
                     <PersonOutlinedIcon sx={{ fontSize: 40 }}/>
                 </Paper>
@@ -38,7 +38,7 @@ export default function AboutMe() {
             <Grid xs={12} md={4}>
                 <Paper sx={baseStyle} style={paperStyle}>
                     <h1>Sobre o Projeto</h1>
-                    <p style={{textAlign: 'justify'}}>
+                    <p style={{textAlign: 'justify', padding: '10px', textIndent: '25px'}}>
                         O projeto foi desenvolvido em React utilizando as bibliotecas <b>Material UI</b> criação dos layouts
                         , <b>Axios</b> para facilitar as requisições, <b>react router</b>  para navegação entre paginas e <b>json-server</b> para criação de um servidor local com as informações de login e escolas.
                     </p>
@@ -48,8 +48,8 @@ export default function AboutMe() {
             <Grid xs={12} md={4}>
                 <Paper sx={baseStyle} style={paperStyle}>
                     <h1>Desafios</h1>
-                    <p style={{textAlign: 'justify'}}>
-                        O maior desafio que encontrei dentro deste trabalho foi de colocar meus conhecimentos em pratica
+                    <p style={{textAlign: 'justify' , padding: '10px' , textIndent: '25px'}}>
+                        O maior desafio que encontrei dentro deste trabalho foi de colocar meus conhecimentos em prática
                         entre as pausas que tive do meu emprego. Acredito que consegui superar esta dificuldade gerenciando 
                         de forma efetiva o tempo, escalando tarefas com prioridade e sempre tirando tempo para planejar as 
                         funções implementadas. 
@@ -60,7 +60,7 @@ export default function AboutMe() {
             <Grid xs={12} md={4}>
                 <Paper sx={baseStyle} style={paperStyle}>
                     <h1>Sugestões</h1>
-                    <p style={{textAlign: 'justify', margin: '20px'}}>
+                    <p style={{textAlign: 'justify', margin: '20px', padding: '10px', textIndent: '25px'}}>
                         Não possuo observações.
                     </p>
                     <LightbulbOutlinedIcon sx={{ fontSize: 40 , marginTop: '30px'}}/>
